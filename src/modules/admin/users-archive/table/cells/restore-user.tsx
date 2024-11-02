c243480f0f0bf1a81d904d1d5359421c1e77e3b6
Nazar <nick20name17@gmail.com>
2024-11-02 04:40:45 +0200

    fix restore cell

--- src/modules/admin/users-archive/table/cells/restore-user.tsx
+++ src/modules/admin/users-archive/table/cells/restore-user.tsx
@@ -1,7 +1,15 @@
+import { Loader2 } from 'lucide-react'
 import { useState } from 'react'
 import { toast } from 'sonner'
 
-import { Checkbox } from '@/components/ui/checkbox'
+import { Button } from '@/components/ui/button'
+import {
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger
+} from '@/components/ui/dialog'
 import { usePatchUserMutation } from '@/store/api/users/users'
 import type { User } from '@/store/api/users/users.types'
 
@@ -10,15 +18,14 @@ interface RestoreUserCellProps {
 }
 
 export const RestoreUserCell = ({ user }: RestoreUserCellProps) => {
-    const [isDeleted, setIsDeleted] = useState(!!user.is_deleted)
+    const [open, setOpen] = useState(false)
+    const [patchUser, { isLoading }] = usePatchUserMutation()
 
-    const [patchUser] = usePatchUserMutation()
-
-    const handlePatchContact = (isDeleted: boolean) => {
+    const handlePatchContact = () => {
         try {
             patchUser({
                 data: {
-                    is_deleted: isDeleted
+                    is_deleted: false
                 },
                 id: user.id
             })
@@ -34,13 +41,49 @@ export const RestoreUserCell = ({ user }: RestoreUserCellProps) => {
     }
 
     return (
-        <Checkbox
-            className='size-5'
-            onCheckedChange={() => {
-                setIsDeleted((prev) => !prev)
-                handlePatchContact(isDeleted)
-            }}
-            checked={isDeleted}
-        />
+        <Dialog
+            open={open}
+            onOpenChange={setOpen}
+        >
+            <DialogTrigger>
+                <Button
+                    variant='outline'
+                    size='sm'
+                >
+                    Відновити
+                </Button>
+            </DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>
+                        Відновити користувача{' '}
+                        <span>{user.first_name + ' ' + user.last_name}</span>?
+                    </DialogTitle>
+                </DialogHeader>
+                <div className='flex items-center justify-end gap-x-4'>
+                    <Button
+                        onClick={() => {
+                            setOpen(false)
+                        }}
+                        size='sm'
+                        variant='secondary'
+                    >
+                        Відмінити
+                    </Button>
+                    <Button
+                        disabled={isLoading}
+                        onClick={handlePatchContact}
+                        size='sm'
+                        className='flex w-24 items-center gap-x-1.5'
+                    >
+                        {isLoading ? (
+                            <Loader2 className='size-4 animate-spin' />
+                        ) : (
+                            'Відновити'
+                        )}
+                    </Button>
+                </div>
+            </DialogContent>
+        </Dialog>
     )
 }
